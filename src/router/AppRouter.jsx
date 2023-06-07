353b6c74865d6949dd4243fb91d466ef582744f9
Paulespinozarl <paulespinozarl@gmail.com>
2023-06-07 16:04:24 -0300

    Mejorando vista al refrescar pagina

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,43 +2,35 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { CalendarPage } from "../calendar"
 import { useAuthStore } from "../hooks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const AppRouter = () => {
-  
-const { status, checkAuthToken } = useAuthStore();
+  const { status, checkAuthToken } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
 
-useEffect(() => {
-  checkAuthToken()
-}, [])
+  useEffect(() => {
+    checkAuthToken().then(() => {
+      setIsLoading(false);
+    });
+  }, []);
 
-
-if ( status === 'checking'){
-  return(
-    <h3>Cargando...</h3>
-  )
-}
+  if (isLoading) {
+    return <h3>Cargando...</h3>;
+  }
 
   return (
     <Routes>
-            {
-                ( status === 'not-authenticated')  
-                    ? (
-                        <>
-                            <Route path="/auth/*" element={ <LoginPage /> } />
-                            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
-                        </>
-                    )
-                    : (
-                        <>
-                            <Route path="/" element={ <CalendarPage /> } />
-                            <Route path="/*" element={ <Navigate to="/" /> } />
-                        </>
-                    )
-            }
-
-        </Routes>
-  )
+      {status === 'not-authenticated' ? (
+        <>
+          <Route path="/auth/*" element={<LoginPage />} />
+          <Route path="/*" element={<Navigate to="/auth/login" replace />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<CalendarPage />} />
+          <Route path="/*" element={<Navigate to="/" replace />} />
+        </>
+      )}
+    </Routes>
+  );
 }
-
-
